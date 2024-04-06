d8bf83818210b80b1ca51bff186fcc9fead8bc19
joaomluis <jooluis2@gmail.com>
2024-04-06 15:35:00 +0100

    Users table

--- src/views/table.jsx
+++ src/views/table.jsx
@@ -1,47 +1,80 @@
 import {
-    Card,
-    CardHeader,
-    CardBody,
-    CardTitle,
-    Table,
-    Row,
-    Col,
-  } from "reactstrap";
-  
-  function DynamicTable() {
-    return (
+  Card,
+  CardHeader,
+  CardBody,
+  CardTitle,
+  Table,
+  Row,
+  Col,
+} from "reactstrap";
 
-        <>
+import useUserStore from "store/useUserStore";
+
+
+
+import { useEffect, useState } from "react";
+
+
+async function getActiveUsers() {
+  const activeUsersRequest =
+    "http://localhost:8080/project_backend/rest/users/all";
+    const token = useUserStore.getState().token;
+
+  try {
+    const response = await fetch(activeUsersRequest, {
+      method: "GET",
+      headers: {
+        Accept: "*/*",
+        "Content-Type": "application/json",
+        token: token,
+      },
+    });
+
+    if (response.ok) {
+      const activeUsers = await response.json();
+
+      console.log("Active users: ", activeUsers);
+      return activeUsers;
+    }
+  } catch (error) {
+    console.error("Failed to fetch categories", error);
+  }
+}
+
+function DynamicTable() {
+
+
+  const [users, setData] = useState([]);
+
+  useEffect(() => {
+    getActiveUsers().then(activeUsers => setData(activeUsers));
+  }, []);
+
+  
+  
+
+ 
+  
+
+  return (
+    <>
       <div className="content">
         <Row>
           <Col md="12">
             <Card>
               <CardHeader>
-                <CardTitle tag="h4">Simple Table</CardTitle>
+                <CardTitle tag="h4">All Users</CardTitle>
               </CardHeader>
+              
               <CardBody>
-                <Table responsive>
-                  <thead className="text-primary">
-                    <tr>
-                      <th>Name</th>
-                      <th>Country</th>
-                      <th>City</th>
-                      <th >Salary</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                   
-                  </tbody>
-                </Table>
+              
               </CardBody>
             </Card>
           </Col>
         </Row>
-        </div>  
-        </>
-
-    );
+      </div>
+    </>
+  );
 }
 
 export default DynamicTable;
-
