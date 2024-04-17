1bf760873b780e3700293ea9246835e66118032a
joaomluis <jooluis2@gmail.com>
2024-04-17 12:40:07 +0100

    added: websocket to chat

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,21 +1,72 @@
 import "react-chat-elements/dist/main.css";
 import "../../assets/css/general-css.css";
+import { useParams } from "react-router-dom";
 
 import React from "react";
 import { Card, CardHeader, CardBody, Row, Col } from "reactstrap";
 import { MessageBox, Input, Button } from "react-chat-elements";
+
+import useUserStore from "../../store/useUserStore";
 
 function Chat() {
   const [input, setInput] = React.useState("");
   const [messages, setMessages] = React.useState([]);
   const [isChatHovered, setIsChatHovered] = React.useState(false);
 
+  //fazer o token para o ws com os usernames das pessoas no chat
+  const { username } = useParams();
+  const myUsername = useUserStore((state) => state.username);
+  const websocketToken = myUsername + "-" + username;
+  
+
+
+
+  const ws = React.useRef(null);
+
+  React.useEffect(() => {
+    ws.current = new WebSocket(
+      `ws://localhost:8080/project_backend/websocket/notifier/${websocketToken}`
+    );
+
+    ws.current.onopen = () => console.log("ws opened");
+    ws.current.onclose = () => console.log("ws closed");
+
+    return () => {
+      ws.current.close();
+    };
+  }, [websocketToken]);
+
+  React.useEffect(() => {
+    if (!ws.current) return;
+
+    ws.current.onmessage = (e) => {
+      try {
+        const message = JSON.parse(e.data);
+        const messageForRendering = {
+          ...message,
+          position: "left",
+          title: "Other User",
+          status: "received",
+        };
+        setMessages((prevMessages) => [...prevMessages, messageForRendering]);
+      } catch (error) {
+        console.error("Error parsing message:", e.data, error);
+      }
+    };
+  }, []);
+
   const handleSend = () => {
-    if (input) {
-      setMessages((prevMessages) => [
-        ...prevMessages,
-        { position: "right", title: "You", text: input, status: "sent" },
-      ]);
+    if (input && ws.current) {
+      const message = { content: input, sender: myUsername, recipient: username};
+      ws.current.send(JSON.stringify(message));
+      const messageForRendering = {
+        text: input,
+        position: "right",
+        title: "You",
+        status: "sent",
+      };
+      setMessages((prevMessages) => [...prevMessages, messageForRendering]);
+
       setInput("");
     }
   };
@@ -39,6 +90,7 @@ function Chat() {
     }, 100);
   }, [messages]);
 
+
   return (
     <>
       <Row>
@@ -58,6 +110,7 @@ function Chat() {
             </CardHeader>
             <CardBody>
               <div
+              
                 className="chat-container"
                 style={{
                   height: "400px",
